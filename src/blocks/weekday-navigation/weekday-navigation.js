5cb2f2aaa3083f346a41a1834bfd61821f1aafdd
Igor <des.yogi@gmail.com>
2024-03-19 14:55:24 +0200

    search-result 1-st preview

---
+++ src/blocks/weekday-navigation/weekday-navigation.js
@@ -0,0 +1,34 @@
+(function () {
+  const weekday = tns({
+    container: '#weekday',
+    //autoplay: true
+    items: 3,
+    slideBy: 'page',
+    autoWidth: true,
+    gutter: 24,
+    controls: false,
+    nav: false,
+    loop: false,
+    mouseDrag: true,
+    startIndex: 0, // 0 - default
+    //center: true,
+    responsive: {
+      360: {
+        gutter: 24,
+        items: 3
+      },
+      768: {
+        gutter: 15,
+        items: 7
+      },
+      992: {
+        gutter: 32,
+        items: 7
+      },
+      1280: {
+        gutter: 75,
+        items: 7
+      }
+    }
+  });
+})()
